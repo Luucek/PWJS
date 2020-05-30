cfb5f8d5ac4d288c5214069b6c05e8f0cf38493a
Patryk W <wszolapatryk@gmail.com>
2020-05-30 13:21:35 +0200

    Add lab06 code

--- lab06/lab06.py
+++ lab06/lab06.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python
 
+import sys
+import os
+
+if len(sys.argv) > 1:
+    path = sys.argv[1]
+else:
+    path = "."
+
+files = os.listdir(path)
+files.sort()
+
+for filename in files:
+    print(filename)
